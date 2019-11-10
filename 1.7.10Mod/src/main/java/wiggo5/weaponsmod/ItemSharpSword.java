81e93db84152454115991f777cd06dca6eb7f140
2019osbjoh <john.osborne151@gmail.com>
2019-11-10 19:35:48 +0000

    ~

--- 1.7.10Mod/src/main/java/wiggo5/weaponsmod/ItemSharpSword.java
+++
@@ -1,8 +0,0 @@
-package wiggo5.weaponsmod;
-
-import net.minecraft.item.Item;
-
-public class ItemSharpSword extends Item
-{
-	
-}
--- 1.7.10Mod/src/main/java/wiggo5/weaponsmod/ItemSharpeningStone.java
+++
@@ -1,7 +0,0 @@
-package wiggo5.weaponsmod;
-
-import net.minecraft.item.Item;
-
-public class ItemSharpeningStone extends Item {
-
-}
