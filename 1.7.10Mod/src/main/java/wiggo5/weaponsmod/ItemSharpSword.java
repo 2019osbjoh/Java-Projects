c439fcc585a022d168d41dce44ea42490ab180ec
2019osbjoh <john.osborne151@gmail.com>
2020-02-08 18:30:54 +0000

    ~

---
+++ 1.7.10Mod/src/main/java/wiggo5/weaponsmod/ItemSharpSword.java
@@ -0,0 +1,8 @@
+package wiggo5.weaponsmod;
+
+import net.minecraft.item.Item;
+
+public class ItemSharpSword extends Item
+{
+	
+}
---
+++ 1.7.10Mod/src/main/java/wiggo5/weaponsmod/item/ItemsharpIronSword.java
@@ -0,0 +1,12 @@
+package wiggo5.weaponsmod.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+
+public class ItemsharpIronSword extends ItemSword 
+{
+	public ItemsharpIronSword(ToolMaterial material)
+	{
+		super(material);
+	}
+}
